5a8339080be968e77a2e0384dce93f9dadfce534
Kent Gruber <kgruber1@emich.edu>
2017-08-27 15:35:32 -0400

    Pass all the tests

--- spec/command_lion_spec.rb
+++ spec/command_lion_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CommandLion do
   it "has a version number" do
     expect(CommandLion::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
