0a2418c4711ce56c53595579f295161a21a1e551
Kent Gruber <kgruber1@emich.edu>
2017-09-03 13:26:22 -0400

    Add more documentation to the option class.

--- lib/command_lion/option.rb
+++ lib/command_lion/option.rb
@@ -1,3 +1,21 @@
 module CommandLion
+
+  # The Option class is a direct sub-class of the Command class. In pretty much
+  # every way it is just a command under the hood. However, instead of being indexed
+  # in an application's commands index, it will be available in whatever command's
+  # options index.
+  #
+  # == Example
+  #
+  #   app = CommandLion::App.build do
+  #     command :example_command do
+  #         # ...
+  #       option :example_option do
+  #         # ...
+  #       end
+  #     end
+  #   end
+  #
+  #   app.commands[:example_command].options[:example_option]
   class Option < Command; end
 end
