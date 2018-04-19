b5ed11bb2ebe300d0f301be9de663c880b3d88be
Kent Gruber <kgruber1@emich.edu>
2018-04-19 15:40:43 -0400

    fix README example in examples to match up

--- examples/readme.rb
+++ examples/readme.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'command_lion'
 require 'pry'
 
+
 CommandLion::App.run do
 
   name "Rainbow Hello"
@@ -10,26 +11,28 @@ CommandLion::App.run do
 
   command :hello do
     description "A simple command to say hello!"
-    
+
     type :string
-    flag "hello"
-    
+
+    flags do
+      short "-h"
+      long  "--hello" 
+    end
+
     default "world"
 
     action do
       puts "Hello #{argument}!"
     end
-    
+
     option :rainbow do
-    	description "STDOUT is much prettier with rainbows!"
-      
-			flag "--rainbow"
-      
-			action do
+      description "STDOUT is much prettier with rainbows!"
+      flag "--rainbow"
+
+      action do
         require 'lolize/auto'
       end
     end
   end
 
 end
-
